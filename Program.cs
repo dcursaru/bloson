1003f0e347c745bb7fd15eb7433881315403c0f5
Dragos C <dragos.cursaru@gmail.com>
2016-04-10 19:57:12 -0400

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+    	Console.WriteLine("Ana are mere");
+      	var policy = new Policy("B12134ACX");
+        Debug.Print("tata are pere");
+        Debug.Print("Hmmmm!");
+    }
+
+      private double CalculateBaseSalary()
+      {
+          return 100.00;
+
+      }
+
+  }
+}
+
+
