a647747c295a0814699cb493e2fc801176084040
Dragos C <dragos.cursaru@gmail.com>
2016-04-10 19:56:45 -0400

    Added Policy.cs

---
+++ Policy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+    internal class Policy
+    {
+        private readonly string _policyNumber;
+
+        public Policy(string policyNumber)
+        {
+            _policyNumber = policyNumber;
+            PolicyNumber = policyNumber;
+        }
+
+        public string PolicyNumber { get; private set; }
+
+        private void Test()
+        {
+            const int age = 10;
+            Debug.Print(Format("My age is {0}", age.ToString()));
+        }
+
+        public void Test(string name)
+        {
+            if (name != null) Debug.Print($"My name is {name}");
+        }
+    }
+}
