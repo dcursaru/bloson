ef9b3b92728d999bf7bd3df8b39f476a74fc950e
dcursaru <dragos.cursaru@gmail.com>
2016-04-10 19:40:51 -0400

    Added files via upload

---
+++ Transition.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApplication
+{
+    internal class Transition
+    {
+    	internal Status From { get; set; }
+        internal Status To { get; set; }
+
+        private void DoTransition()
+        {
+
+        }
+    }
+}
